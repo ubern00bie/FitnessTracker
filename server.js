272b1f25ae2afed94a9b929b10c3eafb09313d6a
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-10-10 22:02:33 -0500

    deploying

--- server.js
+++ server.js
@@ -11,25 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessApp", { 
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
 });
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-// app.get("/exercise", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./exercise.html"));
-// });
-
-// app.get("/stats", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./stats.html"));
-// });
-
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
