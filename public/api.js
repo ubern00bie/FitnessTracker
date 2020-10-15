7e82effe468dff8bb61263ba1f5b3e2393caf2a6
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-10-14 21:16:52 -0500

    additions to readme

--- public/api.js
+++ public/api.js
@@ -13,7 +13,7 @@ const API = {
     async addExercise(data) {
       const id = location.search.split("=")[1];
   
-      const res = await fetch("/api/workouts/" + id, { //error here
+      const res = await fetch("/api/workouts/" + id, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
